233b3a023965c23347fad0ca3cf783070f898632
sh-anna <62933455+sh-anna@users.noreply.github.com>
2020-12-14 21:52:49 +0200

    Campground Index, Show, New& Create

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -21,15 +22,32 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true }))
+
 
 app.get('/', (req, res) => {
     res.render('home')
+});
+app.get('/campgrounds', async (req, res) => {
+    const campground = await Campground.find( {} );
+    res.render('campgrounds/index', { campgrounds })
+});
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async(req, res) => {
+    const camground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!' });
-    await camp.save();
-    res.send(camp)
-})
+
+app.get('/campgrounds/:id', async(req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campgrounds});
+});
+
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
