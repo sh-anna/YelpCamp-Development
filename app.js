8ae98f729908a9151feb95b9862f93313c180739
sh-anna <62933455+sh-anna@users.noreply.github.com>
2020-12-21 14:26:42 +0200

    Compground Delete

--- app.js
+++ app.js
@@ -57,6 +57,12 @@ app.put('/campgrounds/:id', async(req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.camground});
     res.redirect(`/campgrounds/${campground._id}`)
+});
+
+app.delete('/campgrounds/:id',async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 
 
